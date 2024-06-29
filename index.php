462ca807097524a148c0b38b528909294269bab5
lisa_ywl <yourworthlesslife@icloud.com>
2024-06-29 18:28:20 +0300

    basic layout & env config

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Мини-блог</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <h1>Мини-блог</h1>
+    <a>Добавить пост</a>
+    <h2>Список постов:</h2>
+    <ul>
+      <li>
+        hi im a blog post
+      </li>
+    </ul>
+</body>
+</html>
